5a367d922f97ccf285c840f1c73cc20965fffde1
himanshu <himanshubb109@gmail.com>
2024-01-02 01:12:53 +0530

    phase 3 finally done

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,22 @@
-// use serde_json;
-// use std::{env, usize};
-
-// // Available if you need it!
-// // use serde_bencode
-
-// #[allow(dead_code)]
-// fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-//     // If encoded_value starts with a digit, it's a number
-//     if let Some((len, content)) = encoded_value.split_once(':') {
-//         // Example: "5:hello" -> "hello"
-//         if let Ok(len) = len.parse::<usize>() {
-//             let string = content[..len].to_string();
-//             return serde_json::Value::String(string);
-//         }
-//     }
-//     panic!("Unhandled encoded value: {}", encoded_value)
-// }
-
-// // Usage: your_bittorrent.sh decode "<encoded_value>"
-// fn main() {
-//     let args: Vec<String> = env::args().collect();
-//     let command = &args[1];
-
-//     if command == "decode" {
-//         // You can use print statements as follows for debugging, they'll be visible when running tests.
-//         eprintln!("Logs from your program will appear here!");
-
-//         // Uncomment this block to pass the first stage
-//         let encoded_value = &args[2];
-//         let decoded_value = decode_bencoded_value(encoded_value);
-//         println!("{}", decoded_value.to_string());
-//     } else {
-//         eprintln!("unknown command: {}", args[1])
-//     }
-// }
-
-
 use serde_json;
-use std::env;
+use std::{env, usize};
 
 // Available if you need it!
 // use serde_bencode
 
+#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some((len, rest)) = encoded_value.split_once(':') {
-        if let Ok(len) = len.parse::<usize>() {
-            return serde_json::Value::String(rest[..len].to_string());
-        }
+    if encoded_value.chars().next().unwrap().is_digit(10) {
+        // Example: "5:hello" -> "hello"
+        let colon_index = encoded_value.find(':').unwrap();
+        let number_string = &encoded_value[..colon_index];
+        let number = number_string.parse::<i64>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        return serde_json::Value::String(string.to_string());
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
     }
-    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
