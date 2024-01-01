21cee58d0715997963b0a9a66f3064d4b3cda74a
himanshu <himanshubb109@gmail.com>
2024-01-02 00:29:09 +0530

    phase 3 done

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ use std::{env, usize};
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some((len,content))=encoded_value.split_once(':') {
+    if let Some((len, content)) = encoded_value.split_once(':') {
         // Example: "5:hello" -> "hello"
-       if let Ok(len)= len.parse::<usize>(){
+        if let Ok(len) = len.parse::<usize>() {
             let string = content[..len].to_string();
-           return serde_json::Value::String(string);
-       }
-    } 
-        panic!("Unhandled encoded value: {}", encoded_value)
-    
+            return serde_json::Value::String(string.to_string());
+        }
+    }
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
