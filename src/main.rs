71b6d2dd4ab1122ad8eaae6e9d915e4a6af987c7
himanshu <himanshubb109@gmail.com>
2024-01-02 01:06:34 +0530

    tetsing copied code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,52 @@
+// use serde_json;
+// use std::{env, usize};
+
+// // Available if you need it!
+// // use serde_bencode
+
+// #[allow(dead_code)]
+// fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+//     // If encoded_value starts with a digit, it's a number
+//     if let Some((len, content)) = encoded_value.split_once(':') {
+//         // Example: "5:hello" -> "hello"
+//         if let Ok(len) = len.parse::<usize>() {
+//             let string = content[..len].to_string();
+//             return serde_json::Value::String(string);
+//         }
+//     }
+//     panic!("Unhandled encoded value: {}", encoded_value)
+// }
+
+// // Usage: your_bittorrent.sh decode "<encoded_value>"
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//     let command = &args[1];
+
+//     if command == "decode" {
+//         // You can use print statements as follows for debugging, they'll be visible when running tests.
+//         eprintln!("Logs from your program will appear here!");
+
+//         // Uncomment this block to pass the first stage
+//         let encoded_value = &args[2];
+//         let decoded_value = decode_bencoded_value(encoded_value);
+//         println!("{}", decoded_value.to_string());
+//     } else {
+//         eprintln!("unknown command: {}", args[1])
+//     }
+// }
+
+
 use serde_json;
-use std::{env, usize};
+use std::env;
 
 // Available if you need it!
 // use serde_bencode
 
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if let Some((len, content)) = encoded_value.split_once(':') {
-        // Example: "5:hello" -> "hello"
+    if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
-            let string = content[..len].to_string();
-            return serde_json::Value::String(string);
+            return serde_json::Value::String(rest[..len].to_string());
         }
     }
     panic!("Unhandled encoded value: {}", encoded_value)
