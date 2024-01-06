e1b401eabeebb58a90c0997218c2d73a4ec15b9a
himanshu <himanshubb109@gmail.com>
2024-01-06 14:01:41 +0530

    phase-3 done

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,27 @@ use std::{env, usize};
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    //if encoded value satrt with i its integers
+    if let Some(rest) = encoded_value.strip_prefix('i') {
+        if let Some((digits, _)) = rest.split_once('e') {
+            // print!("holla");
+
+            return serde_json::Value::String(digits.to_string());
+        }
+        // let string = &encoded_value[1..encoded_value.len() - 1];
+    }
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    else if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
     }
+    //  else {
+    panic!("Unhandled encoded value: {}", encoded_value);
+    // }
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
